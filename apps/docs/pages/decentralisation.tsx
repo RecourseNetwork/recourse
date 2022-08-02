d4cd7e8db9e5101da2baaa7c2fd7047024b4b048
Shaun Church <shaun@shaunchurch.com>
2022-08-02 10:53:28 +0100

    Add placeholder pages

---
+++ apps/docs/pages/decentralisation.tsx
@@ -0,0 +1,10 @@
+import { Button } from "ui";
+
+export default function Decentralisation() {
+  return (
+    <div>
+      <h1>Decentralisation</h1>
+      <Button />
+    </div>
+  );
+}
---
+++ apps/docs/pages/networks.tsx
@@ -0,0 +1,10 @@
+import { Button } from "ui";
+
+export default function Networks() {
+  return (
+    <div>
+      <h1>Networks</h1>
+      <Button />
+    </div>
+  );
+}
